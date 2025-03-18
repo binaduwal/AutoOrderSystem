40fa1a8cbb19cdfb0a9bc5e000cd3aee75ee1690
Bina Duwal <bina.duwal@sysqube.com.np>
2025-03-18 11:13:03 +0545

    parentId added

--- Backend/models/categoryModel.js
+++ Backend/models/categoryModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
   category_name: { type: String, required: true, unique: true },
-  display_name: { type: String, required: true },
   status: {
     type: String,
     enum: ['active', 'inactive'], 
@@ -11,9 +10,14 @@ const CategorySchema = new mongoose.Schema({
   companyId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'CompanyModel',
-    required: true,
   },
   description: { type: String },
+
+  parentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    default: null
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Category", CategorySchema);
